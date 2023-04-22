43c3028a9db24fed67bd26557a12ddbf1db59c97
Otávio Paganotti <otaviopaganotti@outlook.com>
2023-04-22 16:20:33 -0400

    chore(types): add base types for pokemon responses

---
+++ src/types/index.ts
@@ -0,0 +1,85 @@
+export type Nullable<T = unknown> = T | null;
+
+export interface PokemonItem {
+  name: string;
+  url: string;
+}
+
+export interface Response<T> {
+  count: number;
+  next: Nullable<string>;
+  previous: Nullable<string>;
+  results: T[];
+}
+
+export interface Sprites {
+  back_default: string;
+}
+
+export interface Stat {
+  base_stat: number;
+  effort: number;
+  stat: Species;
+}
+
+export interface Type {
+  slot: number;
+  type: Species;
+}
+
+export interface VersionGroupDetail {
+  level_learned_at: number;
+  move_learn_method: Species;
+  version_group: Species;
+}
+
+export interface Move {
+  move: Species;
+  version_group_details: VersionGroupDetail[];
+}
+
+export interface VersionDetail {
+  rarity: number;
+  version: Species;
+}
+
+export interface Species {
+  name: string;
+  url: string;
+}
+
+export interface HeldItem {
+  item: Species;
+  version_details: VersionDetail[];
+}
+
+export interface GameIndex {
+  game_index: number;
+}
+
+export interface Ability {
+  ability: Species;
+  is_hidden: boolean;
+  slot: number;
+}
+
+export interface Pokemon {
+  abilities: Ability[];
+  base_experience: number;
+  forms: Species[];
+  game_indices: GameIndex[];
+  height: number;
+  held_items: HeldItem[];
+  id: number;
+  is_default: boolean;
+  location_area_encounters: string;
+  moves: Move[];
+  name: string;
+  order: number;
+  past_types: unknown[];
+  species: Species;
+  sprites: Sprites;
+  stats: Stat[];
+  types: Type[];
+  weight: number;
+}
