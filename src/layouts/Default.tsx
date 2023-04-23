63d1dfb013cbc375bf47226dd0f55863b8168350
Otávio Paganotti <otaviopaganotti@outlook.com>
2023-04-23 03:34:23 -0400

    feat(layouts): finalize layout structure

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,5 +1,5 @@
 import type { FC, ReactNode } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 interface DefaultLayoutProps {
   children?: ReactNode;
@@ -8,7 +8,13 @@ interface DefaultLayoutProps {
 const DefaultLayout: FC<DefaultLayoutProps> = ({ children }) => {
   return (
     <>
-      <div>{children || <Outlet />}</div>
+      <nav className="bg-primary p-4 text-white sticky top-0">
+        <header className="flex justify-between">
+          <h1 className="font-bold"><Link to={'/'}>Pokémon Page</Link></h1>
+          <a href="https://github.com/otavio-paganotti/react-pokemon" target="_blank">Github</a>
+        </header>
+      </nav>
+      <main className="h-screen-minus-56 w-screen overflow-x-hidden">{children || <Outlet />}</main>
     </>
   );
 };
