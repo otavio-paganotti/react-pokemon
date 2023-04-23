9913187ea0f0d0a9845b6dd61333e0b5ca0df4ee
Otávio Paganotti <otaviopaganotti@outlook.com>
2023-04-22 21:22:00 -0400

    feat(components/grid): add initial grid component

---
+++ src/components/grid/Grid.tsx
@@ -0,0 +1,13 @@
+import Paginator from './Paginator';
+import List from './List';
+
+const Grid = () => {
+  return (
+    <>
+      <List />
+      <Paginator />
+    </>
+  )
+};
+
+export default Grid;
