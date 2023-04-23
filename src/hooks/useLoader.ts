fa7c8d505713dc8ebbd19766de5b41e00cbfc25a
Otávio Paganotti <otaviopaganotti@outlook.com>
2023-04-23 03:33:48 -0400

    chore(hooks): add hook structure and create `useLoader` hook

---
+++ src/hooks/useLoader.ts
@@ -0,0 +1,30 @@
+// this function will be used for loading data from the server
+
+import { useRef, useState } from "react";
+
+export const useLoader = <T extends unknown>(initialData?: T) => {
+  const data = useRef<T>(initialData as T);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const loader = (callback: () => Promise<T>) => {
+    if (isLoading) return;
+
+    setIsLoading(true);
+
+    return callback()
+      .then((res) => {
+        data.current = res;
+      })
+      .catch((err) => {
+        setErrors([...errors, err.message]);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  return [
+    loader,
+    data.current,
+    isLoading
+  ] as [(callback: () => Promise<T>) => Promise<void> | undefined, T, boolean];
+}
