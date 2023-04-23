efd48c410a39bd6344cb5fdadb3b78b012d732a0
Otávio Paganotti <otaviopaganotti@outlook.com>
2023-04-23 03:39:53 -0400

    feat(pages/pokemon): finalize pokemon page

--- src/pages/pokemon/Index.tsx
+++ src/pages/pokemon/Index.tsx
@@ -1,11 +1,51 @@
+import { Pokemon as PokemonApi } from '@/api';
+import Card from '@/components/ui/Card';
+import { useLoader } from '@/hooks';
+import { Pokemon } from '@/types';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function PokemonPage() {
   const params = useParams();
 
+  const [
+    loader,
+    data,
+    isLoading
+  ] = useLoader<Pokemon>({} as Pokemon);
+
+  useEffect(() => {
+    loader(() => PokemonApi.getPokemon(Number(params.id)));
+  }, [params.id]);
+
+  const getXAttack = () => data.stats?.find(stat => stat.stat.name === 'special-attack')?.base_stat;
+  const getXDefense = () => data.stats?.find(stat => stat.stat.name === 'special-defense')?.base_stat;
+  const getSpeed = () => data.stats?.find(stat => stat.stat.name === 'speed')?.base_stat;
+
   return (
     <>
-      <div>First :id content. ID = {params.id}</div>
+      <div className="p-4 max-w-3xl mx-auto grid grid-cols-1 gap-4">
+        <h1 className="text-4xl text-center">Pokemon</h1>
+        <p className="text-center">This is the Pokemon page</p>
+        {!isLoading && (
+          <>
+            <Card item={
+              ({
+                id: data.id,
+                url: 'dsadasdasdsa/1',
+                name: data.name,
+                content: data
+              })
+            } noLink={true}>
+              <div className="px-2 pb-2">
+                <p>Ataque especial: {getXAttack()}</p>
+                <p>Defesa especial: {getXDefense()}</p>
+                <p>Velocidade: {getSpeed()}</p>
+              </div>
+            </Card>
+          </>
+        )}
+      </div>
     </>
   );
 }
