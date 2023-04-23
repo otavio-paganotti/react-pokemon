bfc9feb99b76df16d8dec60adac483fa87b6e031
Otávio Paganotti <otaviopaganotti@outlook.com>
2023-04-22 20:38:55 -0400

    chore(tests): add first simple tests for pages module

---
+++ tests/pages/pokemon.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import PokemonComponent from '@/pages/pokemon/Index';
+
+const toJson = (component: renderer.ReactTestRenderer) => {
+  const result = component.toJSON()
+  expect(result).toBeDefined()
+  expect(result).not.toBeInstanceOf(Array)
+  return result as renderer.ReactTestRendererJSON
+};
+
+test('', () => {
+  const component = renderer.create(<PokemonComponent />);
+
+  let tree = toJson(component);
+  expect(tree).toMatchSnapshot();
+});
