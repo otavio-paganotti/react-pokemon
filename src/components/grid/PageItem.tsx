644776b0a238bbefdfc0cabc874a3aa8b22d3e23
Otávio Paganotti <otaviopaganotti@outlook.com>
2023-04-23 22:28:50 -0400

    chore(components/grid): split pageItem into separate component

---
+++ src/components/grid/PageItem.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+
+const PageItem = ({ item, page, itemLabel, current }: {
+  item: number;
+  itemLabel: number | string;
+  page?: ((page: number) => void);
+  current?: boolean;
+}) => {
+  return (
+    <div className={clsx('page-item', current ? 'current' : '')} onClick={() => page && page(item)}>
+      {itemLabel}
+    </div>
+  )
+};
+
+export default PageItem;
