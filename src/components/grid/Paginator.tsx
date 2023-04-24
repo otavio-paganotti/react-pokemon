25ba6d1e2c63484ca5316893287a7e45d24b55d0
Otávio Paganotti <otaviopaganotti@outlook.com>
2023-04-23 22:29:25 -0400

    refactor(components/grid): change paginator structure

--- src/components/grid/Paginator.tsx
+++ src/components/grid/Paginator.tsx
@@ -1,48 +1,28 @@
-import clsx from "clsx";
-
-const PageItem = ({ item, page, itemLabel, current }: {
-  item: number;
-  itemLabel: number | string;
-  page?: ((page: number) => void);
-  current?: boolean;
-}) => {
-  return (
-    <div className={clsx('page-item', current ? 'current' : '')} onClick={() => page && page(item)}>
-      {itemLabel}
-    </div>
-  )
-};
+import { IPagination } from "@/types";
+import PageItem from "./PageItem";
 
 const Paginator = ({
   pagination,
   page
 }: {
-  pagination: {
-    firstPage: number;
-    lastPage: number;
-    nextPages: number[];
-    prevPages: number[];
-    nextPage: number;
-    prevPage: number;
-    page: number;
-  },
+  pagination: IPagination,
   page: ((page: number) => void)
 }) => {
   return (
     <div className="flex justify-center mb-4">
       <div className="mb-4">
         <nav className="flex md:gap-4" aria-label="Pagination">
-          <PageItem page={(item) => pagination.page > 1 && page(item)} item={pagination.firstPage} itemLabel="<<" />
-          <PageItem page={(item) => pagination.page > 0 && page(item)} item={pagination.prevPage} itemLabel="<" />
+          <PageItem page={page} item={pagination.firstPage} itemLabel="<<" />
+          <PageItem page={page} item={pagination.prevPage} itemLabel="<" />
           {pagination.prevPages.length > 0 && pagination.prevPages.map((item) => (
-            <PageItem item={item} itemLabel={item} page={page} />
+            <PageItem key={'prev-' + item} item={item} itemLabel={item} page={page} />
           ))}
           <PageItem item={pagination.page} itemLabel={pagination.page} current={true} />
           {pagination.nextPages.length > 0 && pagination.nextPages.map((item) => (
-            <PageItem item={item} itemLabel={item} page={page} />
+            <PageItem key={'next-' + item} item={item} itemLabel={item} page={page} />
           ))}
-          <PageItem page={(item) => pagination.page < pagination.lastPage && page(item)} item={pagination.nextPage} itemLabel=">" />
-          <PageItem page={(item) => pagination.page < pagination.lastPage && page(item)} item={pagination.lastPage} itemLabel=">>" />
+          <PageItem page={page} item={pagination.nextPage} itemLabel=">" />
+          <PageItem page={page} item={pagination.lastPage} itemLabel=">>" />
         </nav>
       </div>
     </div>
